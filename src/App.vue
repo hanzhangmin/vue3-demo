6863ee161a88bb49b488b36a72a927dfd0d3c24c
hanzhangmin <1821898203@qq.com>
2020-10-02 16:20:11 +0800

    frist commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div id="app">
+  {{message}}
+  <button>
+    <router-link to="/"> 首页</router-link>
+  </button>
+  <button>
+    <router-link to="/about"> 我的</router-link>
+  </button>
+
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+
+  <List :list="books" @addlist="addlist"></List>
+</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+</style>
 
-#nav {
-  padding: 30px;
+<script>
+// compositionApi
+import {
+  reactive,
+  toRefs,
+  watch,
+  computed,
+  ref
+} from "vue"
+import {
+  useRoute,
+  useRouter
+} from "vue-router"
+import {
+  useStore
+} from "vuex"
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+import List from './components/List'
+export default {
+  components: {
+    List
+  },
+  // 里面没有this ,默认只执行一次
 
-    &.router-link-exact-active {
-      color: #42b983;
+  // props父组件传递的属性 , context 里面有三个方法 attrs:emit:slots
+  setup(props, context) {
+    const route = useRoute();
+    const state = reactive({
+      message: "hzm",
+      books: ["红楼梦", "水浒传", "三国演义", "西游记"]
+    })
+    watch(route, (newvalue) => {
+      // console.log(route, newvalue);
+    })
+    const addlist = (book) => {
+      state.books.push(book);
+    }
+    return {
+      ...toRefs(state), //   toRefs 将代理封装 ,以实现响应式，还有解构赋值的功能
+      addlist
     }
   }
 }
-</style>
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <ul>
+    <li v-for="(li,index) in list" :key="index+'li'">{{li}}</li>
+  </ul>
+  <input v-model="book" type="text" />
+  <button @click="transfer(book)">add</button>
+</div>
+</template>
+
+<script>
+import {
+  toRefs,
+  reactive
+} from 'vue'
+export default {
+  name: "my-list",
+  props: {
+    list: Array
+  },
+  setup(props, contect) {
+    // console.log(props.list);
+    const state = reactive({
+      book: "汉书"
+    })
+    const transfer = (book) => {
+      contect.emit('addlist', book);
+    }
+    return {
+      ...toRefs(state),
+      transfer
+    }
+  }
+
+}
+</script>
